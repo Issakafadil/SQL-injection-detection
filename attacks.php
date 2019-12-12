1c4ed1d3aa5b41c89743ac80421c620f400ca55e
Issakafadil <issakafadil@gmail.com>
2019-12-12 23:38:55 +0800

    Create attacks.php

---
+++ attacks.php
@@ -0,0 +1,177 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+<title>Registration Form</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style type="text/css">
+	body {
+		color: #fff;
+		background: #581845;
+		font-family: 'Roboto', sans-serif;
+	}
+	.form-control, .form-control:focus, .input-group-addon {
+		border-color: #e1e1e1;
+	}
+    .form-control, .btn {
+        border-radius: 3px;
+    }
+	.signup-form {
+		width: 390px;
+		margin: 0 auto;
+		padding: 30px 0;
+	}
+    .signup-form form {
+		color: #999;
+		border-radius: 3px;
+    	margin-bottom: 15px;
+        background: #fff;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 30px;
+    }
+	.signup-form h2 {
+		color: #333;
+		font-weight: bold;
+        margin-top: 0;
+    }
+    .signup-form hr {
+        margin: 0 -30px 20px;
+    }
+	.signup-form .form-group {
+		margin-bottom: 20px;
+	}
+	.signup-form label {
+		font-weight: normal;
+		font-size: 13px;
+	}
+	.signup-form .form-control {
+		min-height: 38px;
+		box-shadow: none !important;
+	}
+	.signup-form .input-group-addon {
+		max-width: 42px;
+		text-align: center;
+	}
+	.signup-form input[type="checkbox"] {
+		margin-top: 2px;
+	}
+    .signup-form .btn{
+        font-size: 16px;
+        font-weight: bold;
+		background: #19aa8d;
+		border: none;
+		min-width: 140px;
+    }
+	.signup-form .btn:hover, .signup-form .btn:focus {
+		background: #179b81;
+        outline: none;
+	}
+	.signup-form a {
+		color: #fff;
+		text-decoration: underline;
+	}
+	.signup-form a:hover {
+		text-decoration: none;
+	}
+	.signup-form form a {
+		color: #19aa8d;
+		text-decoration: none;
+	}
+	.signup-form form a:hover {
+		text-decoration: underline;
+	}
+	.signup-form .fa {
+		font-size: 21px;
+	}
+	.signup-form .fa-paper-plane {
+		font-size: 18px;
+	}
+	.signup-form .fa-check {
+		color: #fff;
+		left: 17px;
+		top: 18px;
+		font-size: 7px;
+		position: absolute;
+	}
+   p {
+   	text-align: center;
+   }
+	 .error{
+		color:red;
+	}
+	.success{
+		color: green;
+	}
+	.ancho{
+		color: orange;
+	}
+</style>
+
+
+
+</head>
+<body>
+<div class="signup-form">
+    <form action="attacklog.php" method="post">
+		<h2>Login {SQL Injection}</h2>
+		<span></span>
+		<hr>
+        <div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-user"></i></span>
+				<input type="text" class="form-control" name="username" placeholder=" username">
+
+			</div>
+        </div>
+
+		<div class="form-group">
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+				<input type="password" class="form-control" name="psw" placeholder="Password" />
+			</div>
+        </div>
+
+
+		<div class="form-group">
+            <button type="submit"  name="submit" class="btn btn-primary btn-lg">Login</button>
+        </div>
+<hr>
+<!-- <font color="orange"><b><i>We will inject this login app with malicious code and let the model detect the injection</i></b></font> -->
+				<?php
+
+				if(!isset($_GET['msg'])){
+					exit();
+				}else{
+					$typeofMsg =$_GET['msg'];
+
+					if($typeofMsg =="empty"){
+				      echo '<p class ="error">You did not fill in all fields</p>';
+				      exit();
+
+					}elseif($typeofMsg =="success"){
+				      echo '<p class ="success">You have successfully loggedin</p>	<div class="text-center"><a href="logout.php">Logout</a></div>
+							</div>' ;
+				      exit();
+					}else{
+				      echo '<p class ="error">Log in failed, try again</p>';
+				      exit();
+					}
+				}
+
+				?>
+
+    </form>
+
+</body>
+
+
+
+</html>
